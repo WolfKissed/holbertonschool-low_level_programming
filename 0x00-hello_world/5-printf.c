93adba67e12d746489545ae3522899b070fd8da0
WolfKissed <github.com/WolfKissed>
2021-09-30 03:29:44 -0700

    gg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	puts(Mystr);
+	return (0);
+}
