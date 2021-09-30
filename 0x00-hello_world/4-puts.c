6ff6c20dcb1e1c2a58df776188ff4b9b1d196afc
WolfKissed <github.com/WolfKissed>
2021-09-30 02:53:23 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-int puts("Programming is like building a multilingual puzzle"* str);
+#include<stdio.h>
+int main()
+{
+    //string initialisation
+    char Mystr[] = "Programming is like building a multilingual puzzle";
+     
+    puts(Mystr); 
+     
+    return 0;
+}
