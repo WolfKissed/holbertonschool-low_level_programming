ae4fd352eb363158413ae106136305f1ccf340f7
WolfKissed <github.com/WolfKissed>
2021-09-30 03:36:28 -0700

    gg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
-
-	printf(Mystr);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
