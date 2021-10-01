9aff53d37e2613624717b8091f8168daaa0cfbee
WolfKissed <github.com/WolfKissed>
2021-10-01 04:12:39 -0700

    gg

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	else if (lg < 6)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lg);
 	else
-	printf("Last digit of %d is 0 and is 0\n", n);
+	printf("Last digit of %d is %d and is 0\n", n, lg);
 
 	return (0);
 }
