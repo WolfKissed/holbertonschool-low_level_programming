1a52cb0b230575209743f9d456433478684d2a0a
WolfKissed <90924129+WolfKissed@users.noreply.github.com>
2021-10-01 13:14:42 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - Prints the alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+    for(i = 'A'; i <= 'Z'; i++)
+{
+    int lower_i = tolower(i);
+    putchar(lower_i);
+}
+
+    return 0;
+}
