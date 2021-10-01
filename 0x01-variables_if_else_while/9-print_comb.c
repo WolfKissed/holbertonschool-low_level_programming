308a2dbde71e71bb256486e528568eb5d83d711f
WolfKissed <github.com/WolfKissed>
2021-10-01 15:31:48 -0700

    gg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <locale.h>
-#include <unistd.h>
 /**
- * main - Prints the numbers from 0 to 10, seperated by commas
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+int i;
 
 	for (i = 0; i < 10; i++)
 {
-		putchar(i + '0');
-	if (i != 9)
+	putchar(i + '0');
+	if (i < 9)
 {
-	    putchar (',');
-	putchar (' ');
+	putchar(',');
+	putchar(' ');
 }
 }
 	putchar('\n');
-	return (0);
+return (0);
 }
