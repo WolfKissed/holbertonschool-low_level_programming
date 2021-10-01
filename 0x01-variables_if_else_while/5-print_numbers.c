89b0fed235a57834566e7c57163fd912084d633a
WolfKissed <github.com/WolfKissed>
2021-10-01 07:11:17 -0700

    gg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - Prints the alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  int i = 0;
+	for (; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
