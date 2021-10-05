8b63f77521dad4c111f252f05b416376cd2d028f
WolfKissed <github.com/WolfKissed>
2021-10-05 10:30:44 -0700

    gg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * jack_bauer - Prints every minute
+ *
+ * Return: Always 1
+ */
+void jack_bauer(void)
+{
+int i, j, k, h, l, m;
+	for (i = 0; i < 24; i++)
+{
+l = i / 10;
+m = i % 10;
+	for (j = 0; j < 60; j++)
+{
+k = j / 10;
+h = j % 10;
+_putchar(l + '0');
+_putchar(m + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(h + '0');
+_putchar('\n');
+}
+}
+}
