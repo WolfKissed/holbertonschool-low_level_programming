ee4ada71a5ebe19d9662441aaec6291149471114
WolfKissed <github.com/WolfKissed>
2021-10-05 05:46:41 -0700

    gg

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: int
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c  <= 'z' && c >= 'a')
+	{
+		return (1);
+	}
+		return (0);
+}
