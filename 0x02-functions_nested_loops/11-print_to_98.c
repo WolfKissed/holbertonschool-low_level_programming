94d85c02dc32e45804cbd763bd8d1bcc504d495d
WolfKissed <github.com/WolfKissed>
2021-10-05 10:55:05 -0700

    gg

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - prints to 98
+ *
+ *@n :int
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+while (n != 98)
+{
+printf("%d, ", n);
+if (n < 98)
+n++;
+else
+n--;
+}
+printf("98\n");
+}
