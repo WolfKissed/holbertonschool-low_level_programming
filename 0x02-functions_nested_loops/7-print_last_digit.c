03ffcaa600dae3e18a4cc78f610c20e3271dad0d
WolfKissed <github.com/WolfKissed>
2021-10-05 09:10:47 -0700

    gg

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ * @n: int
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+long x = n;
+		if (x < 0)
+{
+	x = -x;
+	return (x%10);
+		else
+{
+	return (x%10);
+}
+}
+return (0);
+}
