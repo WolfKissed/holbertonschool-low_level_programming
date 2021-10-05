2979c68e533148d36ba3d8153048daec4aa630e2
WolfKissed <github.com/WolfKissed>
2021-10-05 05:23:13 -0700

    gg

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,27 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 int _islower(int c)
 {
-    char c;
-    int i;
-    while (i < z)
-    {
-    if (i == c)
-    return (1);
-    i++;
+	if (c  <= 'z' && c >= 'a')
+	{
+		return (1);
+	}
+		return (0);
+	
 }
-    return (0);
-}}
