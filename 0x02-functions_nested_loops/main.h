e136e5df3db2e4781b856008115ec54bb5ea566d
WolfKissed <github.com/WolfKissed>
2021-10-05 05:32:25 -0700

    gg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,14 @@
-#ifndef H
-#define H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <locale.h>
 #include <unistd.h>
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,4 +20,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+int main(void);
 #endif
