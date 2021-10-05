7c88268126880b2febee05a7d1a49ab8e6ebf7f0
WolfKissed <github.com/WolfKissed>
2021-10-05 11:36:01 -0700

    gg

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,17 @@
-#include<stdio.h>
-int fibonacci_series(int);
+#include<stdio.h>    
 /**
  * main - check the code.
  * Return: Always 0.
  */
-int main(void)
-{
-   int c = 0, i;
-
-   for ( i = 1 ; i <= 50 ; i++ )
-   {
-      printf("%d, ", fibonacci_series(c));
-      c++;
-   }
-
-   return 0;
-}
-/**
- * fibonacci_series - check the code.
- * Return: 0.
- */
-int fibonacci_series(int num)
-{
-   if ( num == 0 )
-     return 0;
-   else if ( num == 1 )
-     return 1;
-   else
-     return ( fibonacci_series(num-1) + fibonacci_series(num-2) );
-}
+int main(void)    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ for(i=1;i<51;++i)
+ {    
+  n3=n1+n2;    
+  printf("%d ,",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }    
