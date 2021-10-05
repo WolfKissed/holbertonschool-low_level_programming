e3078e8ff15c4309d043ef18a35611909a5e249c
WolfKissed <github.com/WolfKissed>
2021-10-05 01:35:50 -0700

    gg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - Prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar");
+		printf("\n");
+		return (0);
+}
