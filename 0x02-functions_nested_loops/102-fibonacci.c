abb29420b824a5be9967b23fb007b0ded4c35bc9
WolfKissed <github.com/WolfKissed>
2021-10-06 02:43:01 -0700

    gg

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,14 +6,14 @@
  */
 int main (void)
 {
-int a = 3, i = 0, b = 1, c = 0;
+int b = 1, c = 0,a,i;
 
 for (i = 1; i < 51; i++)
 {
 	a = b + c;
-	b = a - 1;
-	c = a - 2;
 	printf("%d, ", a);
+	c = b;
+	b = a;
 }
 return (0);
 }
