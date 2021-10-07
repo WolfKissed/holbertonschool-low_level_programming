9e677e54653f96b5b53f3a9a640e7c87d6947f80
WolfKissed <github.com/WolfKissed>
2021-10-07 02:52:54 -0700

    gg

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers except two and four
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+	if (i == '2' || i == '4')
+	continue;
+
+	else
+	_putchar(i);
+}
+	_putchar('\n');
+}
