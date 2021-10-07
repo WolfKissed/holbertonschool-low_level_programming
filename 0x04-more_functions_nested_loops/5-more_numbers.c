2c27fd2f141d61604f2a5b109c13a90d05d8dae6
WolfKissed <github.com/WolfKissed>
2021-10-07 03:06:32 -0700

    gg

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers ten times
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int x = 0;
+
+	while (x < 10)
+	{
+
+	for (i = 0; i <= 14; i++)
+	putchar(i);
+	putchar('\n');
+	x++;
+	}
+}
