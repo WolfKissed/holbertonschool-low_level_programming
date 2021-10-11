3532fff63eacc09db2728d02215908737664ba3a
WolfKissed <github.com/WolfKissed>
2021-10-11 12:04:21 -0700

    gg

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints a string
+ * @str: char
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; i <= _strlen(str); i = i + 2)
+{
+printf("s[i]");
+}
+printf("\n");
+}
