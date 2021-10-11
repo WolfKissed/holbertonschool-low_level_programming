ba75fc56d738b7ccaad91412172f6aed1ef624eb
WolfKissed <github.com/WolfKissed>
2021-10-11 02:43:35 -0700

    gg

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strlen - returns the length of a string
  * @s: char
@@ -6,6 +7,6 @@
  */
 int _strlen(char *s)
 {
-int strlen(*s);
+int strlen(s);
 return (0);
 }
