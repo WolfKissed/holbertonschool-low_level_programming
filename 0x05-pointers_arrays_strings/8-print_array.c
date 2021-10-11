78f06ef5808267606066775b20af7df677a13ffc
WolfKissed <github.com/WolfKissed>
2021-10-11 12:13:44 -0700

    gg

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array
+ * @a: int
+ * @n: int
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n -1)
+	contine;
+else
+printf(", ");
+}
+}
