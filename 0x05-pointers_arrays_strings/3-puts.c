041186bebce3ddcf13495959085e2c6185cdc3fa
WolfKissed <github.com/WolfKissed>
2021-10-11 03:32:24 -0700

    gg

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen - returns the length of a string
- * @s: char
+ * _puts - prints a string
+ * @str: char
  * Return: 0
  */
 void _puts(char *str)
