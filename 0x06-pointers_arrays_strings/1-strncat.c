a824377a7094aff7a929b25b73b5ab4f36cd2878
WolfKissed <github.com/WolfKissed>
2021-10-13 09:41:46 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (dest [i] != '\0')
+{
+i++;
+}
+while ((src [j] != '\0') && (j < n))
+{
+dest [i] = src [j];
+i++;
+j++;
+}
+return (dest);
+}
