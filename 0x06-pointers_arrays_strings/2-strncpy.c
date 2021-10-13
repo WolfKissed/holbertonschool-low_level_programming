9f15517e1bc547f7ad1c51c568049f0385aa2d33
WolfKissed <github.com/WolfKissed>
2021-10-13 09:54:40 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+if (i < n)
+dest[i] = src[i];
+for (; i < n; i++)
+	dest[i] = '\0';
+return (dest);
+}
