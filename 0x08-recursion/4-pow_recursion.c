af70c1ff8d50c805d13cfa2b7f64c403d9cbd76b
WolfKissed <github.com/WolfKissed>
2021-10-20 13:13:29 -0700

    gg

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x to the power of y
+ * @x:int
+ * @y:int
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+else if (y == 1)
+return (x);
+else
+return (x * _pow_recursion(x, y - 1));
+}
