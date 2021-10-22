e694c946bdfa76a04a8f19d1d603fad4cc793953
WolfKissed <github.com/WolfKissed>
2021-10-22 14:51:58 -0700

    gg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,9 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-int counter;
-
-for(counter=0; counter<argc; counter++)
-printf("argv[%2d]: %s\n",counter,argv[counter]);
-return 0;
+int i = 0;
+for (; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
 }
