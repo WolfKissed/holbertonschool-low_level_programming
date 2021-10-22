a31d2c1a13497dc9899b60a00d2e708f4613db87
WolfKissed <github.com/WolfKissed>
2021-10-22 14:43:29 -0700

    gg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints a program's name
+ * @argc:int
+ * @argv:char[]
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+_putchar("%s\n", argv[0]);
+return (0);
+}
+
