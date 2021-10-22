cfd4cb395508f465d666f1b988c53ce922f65540
WolfKissed <github.com/WolfKissed>
2021-10-22 14:47:29 -0700

    gg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Prints arguments
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
