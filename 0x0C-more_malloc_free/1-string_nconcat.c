b3ef413bca7b2c8377e5a59bf6503c17301901c0
WolfKissed <github.com/WolfKissed>
2021-10-28 06:04:16 -0700

    gg

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,33 @@
 #include "main.h"
 /**
  * string_nconcat - Concats two strings
+ * @n: int
  * @s1: char
  * @s2: char
- * @n: unsigned int
- *
  * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int length = n, i;
+char *s;
+unsigned int i, j, x, y;
 
 if (s1 == NULL)
-	s1 = "";
+s1 = "";
 if (s2 == NULL)
-	s2 = "";
-for (i = 0; s1[i]; i++)
-	length++;
-c = malloc(sizeof(char) * (len + 1));
-if (c == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
+if (n >= j)
+n = j;
+s = malloc(sizeof(char) * (i + n + 1));
+if (s == NULL)
 return (NULL);
-	length = 0;
-for (i = 0; s1[i]; i++)
-	c[length++] = s1[i];
-for (i = 0; s2[i] && i < n; i++)
-	c[length++] = s2[i];
-	c[length] = '\0';
-return (c);
+for (x = 0; s1[x] != '\0'; x++)
+s[x] = s1[x];
+for (y = x; y < (i + n); y++)
+s[y] = s2[y - x];
+s[y] = '\0';
+return (s);
 }
