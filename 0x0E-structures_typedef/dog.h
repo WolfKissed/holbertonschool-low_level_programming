fa7fe5bb117451d62193a80e9de9474343cbf8ad
WolfKissed <github.com/WolfKissed>
2021-11-01 13:42:23 -0700

    gg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
 /**
  * struct dog - New defined type
  * @name: char
@@ -13,4 +20,9 @@ char *name;
 float age;
 char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
