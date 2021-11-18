1fcc1e1a494249fa12e52487934aa3fa53e3979c
WolfKissed <github.com/WolfKissed>
2021-11-18 06:04:36 -0800

    gg

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - Returns the value of a bit 
+ * @n: long int
+ * @index: long int
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (sizeof(unsigned long int) * 8 < index)
+return (-1);
+n >>= index;
+if ((n & 1) == 1)
+return (1);
+return (0);
+}
