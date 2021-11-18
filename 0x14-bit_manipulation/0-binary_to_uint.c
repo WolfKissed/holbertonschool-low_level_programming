8056a636b8100569f7a98066b4329d551146e412
WolfKissed <github.com/WolfKissed>
2021-11-18 05:40:43 -0800

    gg

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: char
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int n = 0, x = 1;
+int i;
+
+if (!b)
+return (0);
+for (i = 0; b[i];)
+i++;
+for (i -= 1; i >= 0; i--)
+{
+	if (b[i] != '0' && b[i] != '1')
+return (0);
+	n += (b[i] - '0') * x;
+	x *= 2;
+}
+return (n);
+}
