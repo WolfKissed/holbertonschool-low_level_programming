72e6f4781d889ef55e0fe2d2df32afcbb2b1f062
WolfKissed <github.com/WolfKissed>
2021-11-22 05:01:08 -0800

    gg

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to a file.
+ * @filename: char
+ * @text_content: char
+ * Return: 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int op, wr, x = 0;
+
+if (filename == NULL)
+	return (-1);
+
+if (text_content != NULL)
+{
+for (x = 0; text_content[len];)
+	x++;
+}
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+if (op == -1 || wr == -1)
+return (-1);
+	close(op);
+return (1);
+}
