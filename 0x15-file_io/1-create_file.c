23649f2d379436ac15b2f569ee009774311a6c30
WolfKissed <github.com/WolfKissed>
2021-11-22 04:40:25 -0800

    gg

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: char
+ * @text_content: char
+ * Return: 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, x = 0;
+
+if (filename == NULL)
+	return (-1);
+if (text_content != NULL)
+{
+for ( x= 0; text_content[x];)
+	x++;
+}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(o, text_content, x);
+if (op == -1 || wr == -1)
+return (-1);
+	close(op);
+return (1);
+}
